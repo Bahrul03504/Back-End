536ff6d16fb3b491069419ac0584c7acf1421913
Muhamad Bahrul Ulum <ulum2720@gmail.com>
2024-08-26 08:06:46 +0700

    first commit

---
+++ app/Models/Admin.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class Admin extends Authenticatable implements JWTSubject
+{
+    use Notifiable;
+
+    protected $guard = 'admin';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($admin) {
+            $admin->role = $admin->role ?? 'admin';
+        });
+    }
+
+    public function isAdmin()
+    {
+        return $this->role === 'admin';
+    }
+
+    // Metode yang diperlukan untuk JWTSubject
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
